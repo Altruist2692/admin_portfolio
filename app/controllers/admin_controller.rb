cdba44b964a411357c16574d4d510d67f094ad58
Altruist2692 <altruist2692@gmail.com>
2016-03-06 01:12:00 +0530

    rename files to make theme more relavant

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,22 @@
+class AdminController < ApplicationController
+  def index
+  end
+
+  def profile
+  end
+
+  def skills
+  end
+
+  def tools
+  end
+
+  def educations
+  end
+
+  def awards
+  end
+
+  def experiances
+  end
+end
