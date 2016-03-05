a7527e91fbce4caaeed6556390d4414b947ac1b9
Altruist2692 <altruist2692@gmail.com>
2016-03-06 00:28:05 +0530

    theme integration for admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+# You can have the root of your site routed with "root"
+  root to: "staff#index"
+
+  # All routes
+
+
+  get "staff/index"
+  get "staff/enter_attendance"
+  get "staff/enter_marks"
+  get "staff/send_message"
+  get "staff/post_assignment"
+  get "staff/get_payslip"
+  get "staff/check_attendance"
+  get "staff/apply_leave"
+  get "staff/profile"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
